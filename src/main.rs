6a789b2beb077ad6e5cfef4142b0fba732935fb2
Gary Moore <madmangaz@gmail.com>
2021-04-17 12:31:45 +0100

    Initial commit
    Contains a naive implementation of factorial

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use rayon::prelude::*;
+use rug::Integer;
+
+fn parallel_factorial(n: u64) -> String {
+    
+
+    String::from("")
+}
+
+fn factorial(n: u64) -> String {
+    let mut acc = n;
+    for index in 1..n {
+        acc *= index;
+    }
+    acc.to_string()
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.is_empty() {
+        panic!("Failed to supply argument.");
+    }
+    let n: u64 = args[0].parse().expect("Malformed number");
+    println!("factorial({}) = {}", n, factorial(n));
+}
